a42fa88afb939051a7124a53ab0b4479be3373c4
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-03-24 10:25:47 -0400

    Add navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './Header';
 
 const App = () => (
-  <div className="App">
-    <p>Inside App Component</p>
-  </div>
+    <BrowserRouter>
+        <Header />
+    </BrowserRouter>
 );
-
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    // views
+    const showNavigation = () => (
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            <Link to='#' className='navbar-brand'>
+                Logo
+            </Link>
+            <button
+                className='navbar-toggler'
+                type='button'
+                data-toggle='collapse'
+                data-target='#navbarTogglerDemo02'
+                aria-controls='navbarTogglerDemo02'
+                aria-expanded='false'
+                aria-label='Toggle navigation'
+            >
+                <span className='navbar-toggler-icon'></span>
+            </button>
+
+            <div className='collapse navbar-collapse' id='navbarTogglerDemo02'>
+                <ul className='navbar-nav ml-auto mt-2 mt-lg-0'>
+                    <li className='nav-item'>
+                        <Link to='#' className='nav-link'>
+                            Signup
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='#' className='nav-link'>
+                            Signin
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+
+    // render
+    return <header id='header'>{showNavigation()}</header>;
+};
+
+export default Header;
