f9094f38518c808ffb0832f45c20daff1bab6a65
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-04-08 18:21:54 -0400

    add signup state & events

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Signup.css';
 
 const Signup = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        password2: '',
+        successMsg: false,
+        errorMsg: false,
+        loading: false,
+    });
+    const {
+        username,
+        email,
+        password,
+        password2,
+        successMsg,
+        errorMsg,
+        loading,
+    } = formData;
+    /****************************
+     * EVENT HANDLERS
+     ***************************/
+    const handleChange = (evt) => {
+        //console.log(evt);
+        setFormData({
+            ...formData,
+            [evt.target.name]: evt.target.value,
+        });
+    };
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        //console.log(formData);
+    };
+
     /****************************
      * VIEWS
      ***************************/
     const showSignupForm = () => (
-        <form className='signup-form'>
+        <form className='signup-form' onSubmit={handleSubmit}>
             {/* username */}
             <div className='form-group input-group'>
                 <div className='input-group-prepend'>
@@ -15,10 +51,12 @@ const Signup = () => {
                     </span>
                 </div>
                 <input
-                    name=''
+                    name='username'
+                    value={username}
                     className='form-control'
                     placeholder='Username'
                     type='text'
+                    onChange={handleChange}
                 />
             </div>
             {/* email */}
@@ -29,10 +67,12 @@ const Signup = () => {
                     </span>
                 </div>
                 <input
-                    name=''
+                    name='email'
+                    value={email}
                     className='form-control'
                     placeholder='Email address'
                     type='email'
+                    onChange={handleChange}
                 />
             </div>
             {/* password */}
@@ -43,9 +83,12 @@ const Signup = () => {
                     </span>
                 </div>
                 <input
+                    name='password'
+                    value={password}
                     className='form-control'
                     placeholder='Create password'
                     type='password'
+                    onChange={handleChange}
                 />
             </div>
             {/* password2 */}
@@ -56,9 +99,12 @@ const Signup = () => {
                     </span>
                 </div>
                 <input
+                    name='password2'
+                    value={password2}
                     className='form-control'
                     placeholder='Confirm password'
                     type='password'
+                    onChange={handleChange}
                 />
             </div>
             {/* signup button */}
@@ -69,7 +115,7 @@ const Signup = () => {
             </div>
             {/* already have account */}
             <p className='text-center text-white'>
-                Have an account? <a href=''>Log In</a>
+                Have an account? <Link to='/signin'>Log In</Link>
             </p>
         </form>
     );
@@ -82,6 +128,7 @@ const Signup = () => {
             <div className='row px-3 vh-100'>
                 <div className='col-md-5 mx-auto align-self-center'>
                     {showSignupForm()}
+                    <p style={{ color: 'white' }}>{JSON.stringify(formData)}</p>
                 </div>
             </div>
         </div>
