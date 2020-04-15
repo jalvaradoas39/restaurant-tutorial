116fc44e8214b9e7d3e5e842706fefb8fb1dd1b9
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-04-15 16:23:44 -0400

    Install cors, morgan, concurrently & update scripts

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
 const connectDB = require('./database/db');
 
+// middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
 
 connectDB();
-
-
-
-
-
-
-
-
-
 
 const port = process.env.PORT || 5000;
 
