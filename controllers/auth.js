f9b579af192a65208a0fbde9860080902d326acf
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-04-29 17:57:14 -0400

    Add signupController

---
+++ controllers/auth.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+exports.signupController = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({
+                errorMessage: 'Email already exists',
+            });
+        }
+
+        const newUser = new User();
+        newUser.username = username;
+        newUser.email = email;
+
+        const salt = await bcrypt.genSalt(10);
+        newUser.password = await bcrypt.hash(password, salt);
+
+        await newUser.save();
+
+        res.json({
+            successMessage: 'Registration success. Please signin.',
+        });
+    } catch (err) {
+        console.log('signupController error: ', err);
+        res.status(500).json({
+            errorMessage: 'Server error',
+        });
+    }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { signupValidator, validatorResult } = require('../middleware/validator');
+const { signupController } = require('../controllers/auth');
 
-router.post('/signup', signupValidator, validatorResult);
+router.post('/signup', signupValidator, validatorResult, signupController);
 
 module.exports = router;
