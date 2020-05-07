46268ba6a73b90116d747a5780953d6e25e140bb
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-05-07 18:52:07 -0400

    Setup Signin component

--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { showErrorMsg } from '../helpers/message';
+import { showLoading } from '../helpers/loading';
 
 const Signin = () => {
-    return <p>Inside Signin component</p>;
+    const [formData, setFormData] = useState({
+        email: 'jdoe@gmail.com',
+        password: 'abc123',
+        errorMsg: false,
+        loading: false,
+        redirectToDashboard: false,
+    });
+
+    const {
+        email,
+        password,
+        errorMsg,
+        loading,
+        redirectToDashboard,
+    } = formData;
+
+    /****************************
+     * EVENT HANDLERS
+     ***************************/
+    const handleChange = (evt) => {
+        setFormData({
+            ...formData,
+            [evt.target.name]: evt.target.value,
+            errorMsg: '',
+        });
+    };
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+    };
+
+    /****************************
+     * VIEWS
+     ***************************/
+    const showSigninForm = () => (
+        <form className='signup-form' onSubmit={handleSubmit} noValidate>
+            {/* email */}
+            <div className='form-group input-group'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                        <i className='fa fa-envelope'></i>
+                    </span>
+                </div>
+                <input
+                    name='email'
+                    value={email}
+                    className='form-control'
+                    placeholder='Email address'
+                    type='email'
+                    onChange={handleChange}
+                />
+            </div>
+            {/* password */}
+            <div className='form-group input-group'>
+                <div className='input-group-prepend'>
+                    <span className='input-group-text'>
+                        <i className='fa fa-lock'></i>
+                    </span>
+                </div>
+                <input
+                    name='password'
+                    value={password}
+                    className='form-control'
+                    placeholder='Create password'
+                    type='password'
+                    onChange={handleChange}
+                />
+            </div>
+            {/* signin button */}
+            <div className='form-group'>
+                <button type='submit' className='btn btn-primary btn-block'>
+                    Signin
+                </button>
+            </div>
+            {/* already have account */}
+            <p className='text-center text-white'>
+                Don't have an account? <Link to='/signup'>Register here</Link>
+            </p>
+        </form>
+    );
+
+    /****************************
+     * RENDERER
+     ***************************/
+    return (
+        <div className='signin-container'>
+            <div className='row px-3 vh-100'>
+                <div className='col-md-5 mx-auto align-self-center'>
+                    {errorMsg && showErrorMsg(errorMsg)}
+                    {loading && (
+                        <div className='text-center pb-4'>{showLoading()}</div>
+                    )}
+                    {showSigninForm()}
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Signin;
