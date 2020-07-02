6fbf14ca5e1f204c562ecc5e5696c34bfbc89a7d
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-07-01 20:17:03 -0400

    Add action buttons for AdminDashboard component

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -18,10 +18,44 @@ const AdminDashboard = () => {
         </div>
     );
 
+    const showActionBtns = () => (
+        <div className='bg-light my-2'>
+            <div className='container'>
+                <div className='row pb-3'>
+                    <div className='col-md-4 my-1'>
+                        <button className='btn btn-outline-info btn-block'>
+                            <i className='fas fa-plus'> Add Category</i>
+                        </button>
+                    </div>
+
+                    <div className='col-md-4 my-1'>
+                        <button className='btn btn-outline-warning btn-block'>
+                            <i className='fas fa-plus'> Add Food</i>
+                        </button>
+                    </div>
+
+                    <div className='col-md-4 my-1'>
+                        <button className='btn btn-outline-success btn-block'>
+                            <i className='fas fa-money-check-alt'>
+                                {' '}
+                                View Orders
+                            </i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
     /****************************
      * RENDERER
      ***************************/
-    return <section>{showHeader()}</section>;
+    return (
+        <section>
+            {showHeader()}
+            {showActionBtns()}
+        </section>
+    );
 };
 
 export default AdminDashboard;
