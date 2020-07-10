eccbd39c77c29adc6f93f4b0e35c25ee79fb7c05
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-07-09 21:31:42 -0400

    Create addCategoryModal

--- client/src/components/AdminDashboard.js
+++ client/src/components/AdminDashboard.js
@@ -23,7 +23,11 @@ const AdminDashboard = () => {
             <div className='container'>
                 <div className='row pb-3'>
                     <div className='col-md-4 my-1'>
-                        <button className='btn btn-outline-info btn-block'>
+                        <button
+                            className='btn btn-outline-info btn-block'
+                            data-toggle='modal'
+                            data-target='#addCategoryModal'
+                        >
                             <i className='fas fa-plus'> Add Category</i>
                         </button>
                     </div>
@@ -47,6 +51,38 @@ const AdminDashboard = () => {
         </div>
     );
 
+    const showCategoryModal = () => (
+        <div id='addCategoryModal' className='modal'>
+            <div className='modal-dialog modal-dialog-centered modal-lg'>
+                <div className='modal-content'>
+                    <div className='modal-header bg-info text-white'>
+                        <h5 className='modal-title'>Add Category</h5>
+                        <button className='close' data-dismiss='modal'>
+                            <span>
+                                <i className='fas fa-times'></i>
+                            </span>
+                        </button>
+                    </div>
+                    <div className='modal-body my-2'>
+                        <form>
+                            <label className='text-secondary'>Category</label>
+                            <input type='text' className='form-control' />
+                        </form>
+                    </div>
+                    <div className='modal-footer'>
+                        <button
+                            className='btn btn-secondary'
+                            data-dismiss='modal'
+                        >
+                            Close
+                        </button>
+                        <button className='btn btn-info'>Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
     /****************************
      * RENDERER
      ***************************/
@@ -54,6 +90,7 @@ const AdminDashboard = () => {
         <section>
             {showHeader()}
             {showActionBtns()}
+            {showCategoryModal()}
         </section>
     );
 };
