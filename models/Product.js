b35d8816d304570174a7ecaa511d0840fe66b8b8
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-10-15 17:09:28 -0400

    Create Food (part 9)

---
+++ models/Product.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const {ObjectId} = mongoose.Schema;
+
+
+const ProductSchema = new mongoose.Schema({
+    fileName: {
+        type: 'String',
+        required: true
+    },
+    productName: {
+        type: 'String',
+        required: true,
+        trim: true,
+        maxlength: 60
+    },
+    productDesc: {
+        type: 'String',
+        trim: true
+    },
+    productPrice: {
+        type: Number,
+        required: true
+    },
+    productCategory: {
+        type: ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    productQty: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true })
+
+
+const Product = mongoose.model('Product', ProductSchema);
+
+
+module.exports = Product;
