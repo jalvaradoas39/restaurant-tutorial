219eb6fcc594628dc067e5226d9717127ab67629
Jorge Alvarado <jalvaradoas39@outlook.com>
2020-11-18 19:39:15 -0500

    Import packages for redux and create store

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
 
 serviceWorker.unregister();
---
+++ client/src/redux/store.js
@@ -0,0 +1,17 @@
+import { combineReducers, applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+	reducer,
+	initialState,
+	composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
