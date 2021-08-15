609cc212b5c2fd19213c80b0085acefa56024e36
Jorge Alvarado <jalvaradoas39@outlook.com>
2021-08-15 11:40:06 -0400

    Update package.json browsersList to remove warning message

--- client/src/redux/constants/filterConstants.js
+++
@@ -1,1 +0,0 @@
-export const GET_NEW_ARRIVALS = 'GET_NEW_ARRIVALS';
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,14 +5,12 @@ import loadingReducer from './reducers/loadingReducers';
 import messageReducer from './reducers/messageReducers';
 import categoryReducer from './reducers/categoryReducers';
 import productReducer from './reducers/productReducers';
-import filterReducer from './reducers/filterReducers';
 
 const reducer = combineReducers({
 	loading: loadingReducer,
 	messages: messageReducer,
 	categories: categoryReducer,
 	products: productReducer,
-	filters: filterReducer,
 });
 
 const initialState = {};
