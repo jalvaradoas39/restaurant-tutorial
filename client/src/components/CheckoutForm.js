8795c44b30de6abf7e9402f29bc2a3de5b9c326b
Jorge Alvarado <jalvaradoas39@outlook.com>
2023-05-05 13:20:26 -0400

    Handle submission of Stripe checkout form

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,10 +1,51 @@
-import { PaymentElement } from '@stripe/react-stripe-js';
+import { useNavigate } from 'react-router-dom';
+import {
+	useStripe,
+	useElements,
+	PaymentElement,
+} from '@stripe/react-stripe-js';
 
 const CheckoutForm = () => {
+	const navigate = useNavigate();
+
+	const stripe = useStripe();
+	const elements = useElements();
+
+	const handleSubmit = async event => {
+		// We don't want to let default form submission happen here,
+		// which would refresh the page.
+		event.preventDefault();
+
+		if (!stripe || !elements) {
+			// Stripe.js hasn't yet loaded.
+			// Make sure to disable form submission until Stripe.js has loaded.
+			return;
+		}
+
+		const result = await stripe.confirmPayment({
+			//`Elements` instance that was used to create the Payment Element
+			elements,
+			redirect: 'if_required',
+		});
+
+		if (result.error) {
+			// Show error to your customer (for example, payment details incomplete)
+			console.log(result.error.message);
+		} else {
+			navigate('/', {
+				state: {
+					result,
+				},
+			});
+		}
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<PaymentElement />
-			<button className='btn btn-primary mt-3'>Submit</button>
+			<button className='btn btn-primary mt-3' disabled={!stripe}>
+				Submit
+			</button>
 		</form>
 	);
 };
